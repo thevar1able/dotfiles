0850298a2c058cf57914d39b0b2332e0e57a116e
Konstantin Bogdnaov <var1able@var1able.ru>
2021-07-18 19:28:40 +0300

    [st] Tune to the new machine specifics

--- st/config.h
+++ st/config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "DejaVu Sans Mono:size=10:antialias=true:autohint=false";
+static char *font = "DejaVu Sans Mono:size=12:antialias=true:autohint=true";
 static int borderpx = 2;
 float alpha = 0.85;
 
