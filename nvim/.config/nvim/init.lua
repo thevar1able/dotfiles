1c2e1fbf8f35df73408e2e5c61b1721283271086
Konstantin Bogdanov <var1able@var1able.ru>
2022-12-26 17:01:22 +0100

    [nvim] Add input modification stuff

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -44,6 +44,12 @@ require('packer').startup(function(use)
     after = 'nvim-treesitter',
   }
 
+  -- Input modification stuff
+  use 'tpope/vim-surround'
+  use 'tpope/vim-sensible'
+  use 'tpope/vim-unimpaired'
+  use 'jiangmiao/auto-pairs'
+
   -- Git related plugins
   use 'tpope/vim-fugitive'
   use 'tpope/vim-rhubarb'
@@ -55,9 +61,12 @@ require('packer').startup(function(use)
   use 'lukas-reineke/indent-blankline.nvim' -- Add indentation guides even on blank lines
   use 'numToStr/Comment.nvim' -- "gc" to comment visual regions/lines
   use 'tpope/vim-sleuth' -- Detect tabstop and shiftwidth automatically
-  use 'junegunn/fzf.vim' -- fzf
+
+  -- Copilot
+  use 'github/copilot.vim'
 
   -- Fuzzy Finder (files, lsp, etc)
+  use 'junegunn/fzf.vim'
   use { 'nvim-telescope/telescope.nvim', branch = '0.1.x', requires = { 'nvim-lua/plenary.nvim' } }
 
   -- Fuzzy Finder Algorithm which requires local dependencies to be built. Only load if `make` is available
@@ -393,7 +402,7 @@ cmp.setup {
     ['<C-Space>'] = cmp.mapping.complete(),
     ['<CR>'] = cmp.mapping.confirm {
       behavior = cmp.ConfirmBehavior.Replace,
-      select = true,
+      select = false,
     },
     ['<Tab>'] = cmp.mapping(function(fallback)
       if cmp.visible() then
