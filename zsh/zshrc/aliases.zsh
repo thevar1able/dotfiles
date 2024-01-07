ef5853556b3b605287e8f6b02ac446cc028179ea
Konstantin Bogdanov <var1able@var1able.ru>
2024-01-07 01:10:19 +0100

    [zsh] Remove redundant aliases

--- zsh/zshrc/aliases.zsh
+++ zsh/zshrc/aliases.zsh
@@ -1,8 +1,5 @@
 alias subl="subl3"
 
-# Needed when ssh-ing to servers without my terminal's terminfo (alacritty and termite)
-alias ssh="TERM=xterm-256color ssh"
-
 cheat() {
     # https://github.com/chubin/cheat.sh
     # `cheat tool` or `cheat language/term+with+pluses[/1..]` (append number for next result)
